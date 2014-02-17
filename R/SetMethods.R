53ae2fce38ec24f590c63201badbbeeea2e665d4
Greg Finak <gfinak@fhcrc.org>
2014-02-17 13:55:53 -0800

    New github repo

---
+++ R/SetMethods.R
@@ -0,0 +1,275 @@
+map<-function(z,...){
+apply(z,1,function(x){w<-(which(x==max(x)));if(length(w)!=0){w}else{NA}})
+}
+setMethod("merge",signature=signature(x="flowObj",y="missing"),function(x,metric="entropy",...){
+    metric<-match.arg(metric,c("entropy","mahalanobis"));
+  k <- x@K;
+  resultObject <- as.list(rep(0,k))
+  resultObject[[k]] <- as(x,"flowMerge");
+  resultObject[[k]]@entropy <- -2*sum(resultObject[[k]]@z*log(resultObject[[k]]@z,base=2), na.rm=T)
+  d <- dim(resultObject[[k]]@mu)[1];
+  ncl<-dim(resultObject[[k]]@mu)[2]
+  for(n in 1:d){
+	resultObject[[k]]@mtree<-graph::addNode(as.character(n),resultObject[[k]]@mtree)
+  }
+	#set the population names for the initial BIC clustering
+	rownames(resultObject[[k]]@mu)<-as.character(1:d)
+	
+  s <- apply(resultObject[[k]]@sigma,1,function(x){xx<-eigen(x);list(solve(xx$vectors%*%sqrt(diag(xx$values,ncl,ncl))%*%t(xx$vectors),LINPACK=TRUE));});
+  #resultObject[[k]]@ssd<- 2*sum(sapply(1:d,function(i)sapply(1:d,function(j)sqrt(sum((s[[i]][[1]]%*%resultObject[[k]]@mu[i,]-s[[j]][[1]]%*%resultObject[[k]]@mu[j,])^2)))))
+
+  if(k > 2){
+    for (kk in (k-1):2) {
+        tmp<-mergeClusters(resultObject[[kk+1]],metric=metric)
+        resultObject[[kk]]<-mergeClusters2(resultObject[[kk+1]],tmp[[1]],tmp[[2]])
+        resultObject[[kk]]@merged<-unlist(lapply(list(Map(resultObject[[kk]])),function(x)length(which(x==tmp[[1]]))))
+        #resultObject@merged <- c(resultObject[[kk+1]]@merged,list(tmp[[1]],tmp[[2]]))
+        message("Merged to ",kk," clusters");
+    }
+    tmp<- mergeClusters(resultObject[[2]],metric=metric)
+    resultObject[[1]]<-mergeClusters2(resultObject[[2]],tmp[[1]],tmp[[2]])
+    resultObject[[1]]@merged<-unlist(lapply(list(Map(resultObject[[1]])),function(x)length(which(x==tmp[[1]]))))
+    message("Merged to 1 clusters");
+  }else if(k==2){
+    #resultObject[[1]] <- mergeClusters(resultObject[[2]], getData(resultObject[[k]])) 
+    tmp<- mergeClusters(resultObject[[2]],metric=metric)
+    resultObject[[1]]<-mergeClusters2(resultObject[[2]],tmp[[1]],tmp[[2]])
+    resultObject[[1]]@merged<-unlist(lapply(list(Map(resultObject[[1]])),function(x)length(which(x==tmp[[1]]))))
+    message("Merged to 1 clusters");
+  }
+    message("Updating model statistics");
+    for(i in 1:length(resultObject)){
+	    resultObject[[i]]<-updateU(resultObject[[i]]);
+        #resultObject[[i]]<-flagOutliers(resultObject[[i]]);
+        ruleOutliers(resultObject[[i]])<-list(x@ruleOutliers)
+        message("Updated model ",i);
+    }
+#    resultObject <- lapply(resultObject, updateU);
+#    resultObject <- lapply(resultObject, flagOutliers);
+#    resultObject <- lapply(resultObject,function(x){ruleOutliers(x)<-list(level=0.9);x});
+    resultObject;
+})
+
+setMethod("split",signature=signature(x="flowMerge",f="missing"),function(x,f, drop = FALSE, population=NULL,split = NULL, rm.outliers = TRUE, ...){
+    split( x = getData(x),f = as(x, "flowClust"), population=population, split = split, rm.outliers = rm.outliers, ...);
+});
+
+.computeU<-function(res,data){
+    u<-matrix(0,dim(res@z)[1],res@K);
+	if(any(res@lambda!=1)){
+    for(i in (1:res@K)){
+    u[,i]<-(res@nu+dim(res@mu)[2])/(res@nu+dmvt((data),res@mu[i,],res@sigma[i,,],res@nu,lambda=res@lambda)$md);
+    #message("Updated cluster ",i);
+    }
+	}else{
+		for(i in (1:res@K)){
+	    u[,i]<-(res@nu+dim(res@mu)[2])/(res@nu+dmvt((data),res@mu[i,],res@sigma[i,,],res@nu)$md);
+	    #message("Updated cluster ",i);
+	    }
+	}	
+    u;
+   }
+
+setGeneric("updateU", function(object){standardGeneric("updateU")});
+setMethod("updateU", signature = signature(object = "flowMerge"), function(object){
+  p<-object@K;
+  q <- object@varNames;
+  #q <- dim(o@mu)[2];
+#  if(length(object@lambda)==1){#test whether the transformation parameter is 1  (length 0)
+#    dprime <- flowClust::box(exprs(object@DATA[["1"]])[,q],object@lambda);
+#  }else{
+   
+    dprime<-exprs(object@DATA[["1"]])[,q]
+#  }
+  #if(require(multicore)&require(doMC)){
+   # registerDoMC();
+    #u<-do.call(cbind,mclapply(as.list(1:object@K),function(i){s<-solve(object@sigma[i,,],LINPACK=TRUE);apply(dprime,1,function(x)(x-object@mu[i,])%*%s%*%(x-object@mu[i,]))}))
+     u<-.computeU(object,dprime);
+ #    if(!is.infinite(object@nu)){
+ #     object@u <- (length(q)+object@nu)/(u+object@nu)
+ #    }else{
+    object@u<-u;
+ #    }
+
+#  }else{
+#      u<-sapply(1:object@K,function(i){s<-solve(object@sigma[i,,],LINPACK=TRUE);apply(dprime,1,function(x)(x-object@mu[i,])%*%s%*%(x-object@mu[i,]))})
+#     if(!is.infinite(object@nu)){
+#      object@u <- (length(q)+object@nu)/(u+object@nu)
+#     }else{
+#    object@u<-u;
+#     }
+#  }
+  object@label <- Map(object);
+  object;
+})
+
+setMethod("getData",signature=signature(obj="flowObj"),function(obj){obj@DATA[["1"]]});
+setMethod("getData",signature=signature(obj="flowMerge"),function(obj){getData(as(obj,"flowObj"))});
+setMethod("plot",signature=signature(x="flowObj",y="missing"),
+          function(x,new.window=TRUE,...){
+            #TODO include some code so that the dimension names are chosen correctly.
+            if(length(x@varNames)>2){
+                comb<-as.matrix(combn(x@varNames,2));
+            }else{
+                comb<-matrix(x@varNames)
+            }
+            if(new.window){
+            par(mfrow=c(ceiling(sqrt(dim(comb)[2])),ceiling(sqrt(dim(comb)[2]))))
+            }
+            if(length(x@varNames)>2){
+                apply(comb,2,function(i){
+                    selectMethod("plot",signature=c(x="flowClust",y="missing"))(as(x,"flowClust"),data=getData(x),subset=i,...);
+             })
+            }else{
+                selectMethod("plot",signature=c(x="flowClust",y="missing"))(as(x,"flowClust"),data=getData(x),...);
+            }
+          })
+
+setMethod("plot",signature=signature(x="flowMerge",y="missing"),
+          function(x,new.window=FALSE,...){
+            #TODO include some code so that the dimension names are chosen correctly.
+            if(length(x@varNames)>2){
+                comb<-as.matrix(combn(x@varNames,2));
+            }else{
+                comb<-matrix(x@varNames)
+            }
+            if(new.window){
+            par(mfrow=c(ceiling(sqrt(dim(comb)[2])),ceiling(sqrt(dim(comb)[2]))))
+            }
+            if(length(x@varNames)>2){
+                apply(comb,2,function(i){
+                    selectMethod("plot",signature=c(x="flowClust",y="missing"))(as(x,"flowClust"),data=getData(x),subset=i,...);
+             })
+            }else{
+                selectMethod("plot",signature=c(x="flowClust",y="missing"))(as(x,"flowClust"),data=getData(x),...);
+            }
+          })
+          
+setGeneric("fitPiecewiseLinreg",function(x,plot=FALSE,normalized=TRUE,...){standardGeneric("fitPiecewiseLinreg")})
+
+setMethod("fitPiecewiseLinreg",signature=signature(x="list"),{
+    function(x,plot=FALSE,normalized=TRUE,...){
+        if(!all(unlist(lapply(x,function(y)is(y,"flowMerge"))))){
+            stop("x is not a valid list of flowMerge objects");        
+        }
+		#@merged slot holds the number of events merged at that point
+        entropy<-flowMerge:::ENT(x);
+        N<-(unlist(lapply(x,function(y)y@merged)))
+        N<-cumsum(c(0,N[-length(N)]))
+        K<-unlist(lapply(x,function(y)y@K));
+        l<-length(entropy);
+    
+        #Two cases.. if there's more than two clusters, or if there's just two clusters. 
+        #If there are more than two clusters.. we can fit a changepoint model.
+        if(l>3){
+            #Try all positions for the changepoint, from 2..(l-1), and compute the sum of squared residuals
+            if(normalized){
+                r<-sapply(2:(l-1),function(b) try(sum(lm(entropy~N,subset=c(1:b))$residuals^2)+sum(lm(entropy~N,subset=c(b:l))$residuals^2)))
+            }else{
+                r<-sapply(2:(l-1),function(b) try(sum(lm(entropy~K,subset=c(1:b))$residuals^2)+sum(lm(entropy~K,subset=c(b:l))$residuals^2)))
+            }
+            r<-as.numeric(r);
+            if(normalized){
+                r2<-sum(lm(entropy~N)$residuals^2);
+            }else{
+                r2<-sum(lm(entropy~K)$residuals^2);
+            }
+            #bic<-c(l*log(r2/l)+2*log(l),l*log(r/l)+2*log(l));        
+            #m<-which.min(bic);        
+            m<-which.min(c(r2,r))
+	    if(plot){
+                if(normalized){
+                    c1<-coefficients(lm(entropy~N,subset=c(1:m)));c2<-coefficients(lm(entropy~N,subset=c(m:l)))
+                    color<-rep(1,l);
+                    color[m]<-2;
+                    plot(N,entropy,col=color,pch=20,main="Entropy of Clustering",xlab="Cumulative Number of Merged Observations",ylab="Entropy");
+                    lines(N[1:m],N[1:m]*c1[2]+c1[1],col="red");
+                    lines(N[m:l],N[m:l]*c2[2]+c2[1],col="red");
+                }else{
+                    c1<-coefficients(lm(entropy~K,subset=c(1:m)));c2<-coefficients(lm(entropy~K,subset=c(m:l)))
+                    color<-rep(1,l);
+                    color[m]<-2;
+                    plot(K,entropy,col=color,pch=20,main="Entropy of Clustering",xlab="Number of Clusters",ylab="Entropy");
+                    lines(K[1:m],K[1:m]*c1[2]+c1[1],col="red");
+                    lines(K[m:l],K[m:l]*c2[2]+c2[1],col="red");
+                }         
+            }
+            return(m);
+            if(m==1){
+                warning("No changepoint found. Returning max BIC solution");
+                return(l);
+            }else{
+                return(m);
+            }        
+        }else if(l==2){
+            warning("Two clusters.. no changepoint possible. Returning fully merged solution");
+			return(1);
+        }else if(l==3)
+        {
+            if(normalized){
+                r2<-sum(lm(entropy~N)$residuals^2);
+                r<-sapply(2:(l-1),function(b) try(list(lm(entropy~N,subset=c(1:b)), lm(entropy~I(1:l),subset=c(b:l)))));
+            }else{
+                r2<-sum(lm(entropy~K)$residuals^2);
+                r<-sapply(2:(l-1),function(b) try(list(lm(entropy~K,subset=c(1:b)), lm(entropy~I(1:l),subset=c(b:l)))));
+            }
+            a<-coefficients(r[[1]])[2];
+            b<-coefficients(r[[2]])[2];
+            angle<-atan(abs(a-b)/(1+a*b))*180/pi
+            if(angle>1){
+                warning("Possible changepoint detected by angle between line segments")
+                return(2)                            
+            }else{
+                warning("No changepoint detected, returning max bic solution")
+                return(3);            
+            }
+        }else if(l==1){
+         return(1);   
+        }
+    }
+})   
+
+setMethod("summary",signature=signature("flowObj"),
+function(object,...){
+    summary(as(object,"flowClust"));
+    print(summary(object@uncertainty));
+    cat("Data summary:\n");
+    summary(getData(object));    
+})
+setMethod("summary",signature=signature("flowMerge"),
+function(object,...){
+    cat("** Merging Information **\n");
+    cat("Entropy of clustering: ",object@entropy,"\n");
+    cat("Normalized Entropy of clustering: ",NENT(list(object)),"\n");
+    summary(as(object,"flowObj"));
+})
+
+setGeneric("flagOutliers",function(object,...){standardGeneric("flagOutliers")});
+setMethod("flagOutliers",signature=signature("flowMerge"),
+function(object,...){
+    if(object@ruleOutliers[1]==0){  
+	      qq<-qf(object@ruleOutliers[2],length(object@varNames),object@nu)
+		  qq<-(object@nu+length(object@varNames))/(object@nu+length(object@varNames)*qq)
+        object@flagOutliers<-sapply(1:nrow(object@u),function(i){
+        object@u[i,object@label[i]]<qq||object@z[i,object@label[i]]<object@ruleOutliers[3]})
+    }else if(object@ruleOutliers[1]==1){
+        object@flagOutliers<-sapply(1:nrow(object@u),function(i){
+        object@flagOutliers<-object@u[i,object@label[i]]<object@ruleOutliers[2]||object@z[i,object@label[i]]<object@ruleOutliers[3]
+	})
+}
+    object;
+})
+
+setMethod("show",signature=signature("flowObj"),
+function(object){
+       cat("Object of class 'flowObj'\n");
+       cat("This object has the following slots:\n");
+       cat("DATA, expName, varNames, K, w, mu, sigma, lambda, nu, z, u, label, uncertainty, ruleOutliers, flagOutliers, rm.min, rm.max, logLike, BIC, ICL\n");
+});
+
+setMethod("show",signature=signature("flowMerge"),
+function(object){
+       cat("Object of class 'flowMerge'\n");
+       cat("This object has the following slots:\n");
+       cat("merged, entropy, DATA, expName, varNames, K, w, mu, sigma, lambda, nu, z, u, label, uncertainty, ruleOutliers, flagOutliers, rm.min, rm.max, logLike, BIC, ICL\n");
+});
---
+++ R/flowMerge.R
@@ -0,0 +1,334 @@
+
+countModes<-function(x){
+    if(class(x)=="matrix"|class(x)=="vector"){
+	x<-as.matrix(x);
+	if(ncol(x)>4){
+	 stop("Can only do mode counting in four or fewer dimensions simultaneously")
+	}
+    }
+    tmp<-feature:::featureSignif(x)
+    sum(diff(which(tmp$curv)[sapply(tmp$fhat$x.grid[[1]][which(tmp$curv)],function(x)x>range(tmp$x)[1]&x<range(tmp$x)[2])])>1)+1
+}
+
+checkForRemoteErrors <- function(val)
+    {
+        count <- 0
+    firstmsg <- NULL
+    for (v in val) {
+        if (inherits(v, "try-error")) {
+            count <- count + 1
+            if (count == 1) 
+                firstmsg <- v
+        }
+    }
+    if (count == 1) 
+        print(paste("one node produced an error: ", firstmsg))
+    else if (count > 1) 
+        print(paste(count, " nodes produced errors; first error: ", 
+            firstmsg))
+    val
+}
+
+initPFlowMerge<-function(cl){
+    if(!is(cl,"cluster")){
+        stop("cl must be a SNOW cluster");
+    }
+    r<-clusterEvalQ(cl,library(flowMerge));
+    if(any(unlist(lapply(r,function(x)is(x,"try-error"))))){
+        print(r);
+     stop("Some cluster nodes failed to initialize correctly");   
+    }
+}
+
+pFlowClust<-function(flowData,cl,K=1:15,B.init=100,tol.init=1e-2,tol=1e-5,B=1000,randomStart=50,nu=4,nu.est=1,trans=1,varNames=NA){
+if(any(is.na(varNames))){
+stop("varNames must be defined")
+}
+if(!is(cl,"cluster")){
+stop("cl must be an object of type cluster from the snow package");
+}
+if(is(flowData,"list")){
+if(all(unlist(lapply(flowData,function(x)is(x,"flowFrame"))))){
+result<-lapply(flowData,function(d)clusterMap(cl,function(...)try(flowClust(...)),list(d),varNames=list(varNames),K=K,B=B,tol.init=tol.init,tol=tol,B.init=B.init,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans));
+}}
+else if(is(flowData,"flowSet")){
+result<-fsApply(flowData,function(d)clusterMap(cl,function(...)try(flowClust(...)),list(d),varNames=list(varNames),K=K,B=B,tol.init=tol.init,tol=tol,B.init=B.init,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans));
+}
+else if(is(flowData,"flowFrame")){
+result<-clusterMap(cl,function(...)try(flowClust(...)),list(flowData),varNames=list(varNames),K=K,B=B,tol.init=tol.init,tol=tol,B.init=B.init,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans)
+}
+else{
+stop("flowData should be a flowFrame, list of flowFrames, or flowSet");
+}
+result;
+}
+
+pFlowMerge<-function(flowData,cl,K=1:15,B.init=100,tol.init=1e-2,tol=1e-5,B=500,randomStart=10,nu=4,nu.est=0,trans=1,varNames=NA){
+    if(!is.null(cl)){
+        initPFlowMerge(cl);
+        result<-pFlowClust(flowData,cl,varNames=varNames,K=K,B.init=B.init,tol.init=tol.init,tol=tol,B=B,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans);    
+    }
+    if(is.null(cl)){
+        if(is(flowData,"list")|is(flowData,"flowSet")){
+            result<-lapply(as(flowData,"list"),function(x)mapply(function(...)try(flowClust(...)),list(x),varNames=list(varNames),K=K,B.init=B.init,tol.init=tol.init,tol=tol,B=B,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans));
+        }else if(is(flowData,"flowFrame")){
+          result<-mapply(function(...)try(flowClust(...)),list(flowData),varNames=list(varNames),K=K,B.init=B.init,tol.init=tol.init,tol=tol,B=B,randomStart=randomStart,nu=nu,nu.est=nu.est,trans=trans)
+        }else{
+            stop("flowData must be a flowFrame, list of flowFrames or flowSet")
+        }   
+    }
+    ##Extract the max BIC objects
+    if(is(result,"list")){##This nested code tests if the output is a list of lists of flowClust results. Inelegant, maybe, but that's R for you.
+        if(all(unlist(lapply(result,function(x)is(x,"list"))))){
+            if(all(unlist(lapply(result,function(x)lapply(x,function(y)is(y,"flowClust")))))){
+                tmp<-lapply(result,function(x)x[[which.max(BIC(x))]]);
+                ##result should be a list of flowClust objects, one per flowFrame input
+                o<-sapply(1:length(tmp),function(i)flowObj(tmp[[i]],flowData[[i]]));
+                m<-lapply(o,function(x)merge(x));
+                result<-lapply(m,function(x){i<-fitPiecewiseLinreg(x,plot=T);x[[i]]});
+            }else{
+                stop("The result must be the output from a parallel call to flowClust");   
+            }
+        }else{
+            if(all(unlist(lapply(result,function(x)is(x,"flowClust")|is(x,"try-error"))))){
+                o<-flowObj(result[[which.max(BIC(result))]],flowData);
+                m<-merge(o);
+                i<-fitPiecewiseLinreg(m,plot=T);
+                result<-m[[i]];
+            }else{
+                stop("oops! The input to pFlowMerge from pFlowClust is incorrect.. neither a list of lists of flowClust objects, nor a list of flowClust objects");   
+            }   
+        }
+    }    
+    result;
+}
+
+
+#cl<-makeSOCKcluster(c(rep("finakg@got04",4),rep("recherche@gotsrv02",8),rep("recherche@gotsrv03",8)))
+#clusterEvalQ(cl,library(flowClust))
+#clusterEvalQ(cl,source("/Users/recherche/merge.R"));
+
+flowObj<-function(flowC=NULL,flowF=NULL){
+  if(!is(flowC,"flowClust")){
+    stop("flowC must be a flowClust object");
+  }
+  if(!is(flowF,"flowFrame")){
+    stop("flowF must be a flowFrame object");
+  }
+  o <- new("flowObj",flowC);
+  o@DATA=new.env(hash=TRUE,parent=emptyenv());
+  o@DATA[["1"]]<-flowF;
+  o;
+}
+
+
+mergeClusters2 <- function(object, a, b){
+	py<-dim(object@mu)[1]
+	dy<-dim(object@mu)[2]
+    #popnames to merge
+	pnames<-rownames(object@mu)[c(a,b)]
+	mpname<-paste(pnames,collapse=":");
+	#fix rownames
+	rn<-rownames(object@mu)
+	rn[a]<-mpname
+	rownames(object@mu)<-rn
+	#update the tree
+	object@mtree<-graph::addNode(mpname,object@mtree);
+	object@mtree<-graph::addEdge(mpname,pnames[1],object@mtree)
+	object@mtree<-graph::addEdge(mpname,pnames[2],object@mtree)
+	
+    # update mu, sigma, w
+    P <- object@w[a]+object@w[b];
+    MU<-(object@w[a]*object@mu[a,]+object@w[b]*object@mu[b,])/P
+    if(is.infinite(object@nu)){
+        SIGMA<-(object@w[a]*(object@mu[a,]%*%t(object@mu[a,])+(object@sigma[a,,])+object@w[b]*(object@mu[b,]%*%t(object@mu[b,])+(object@sigma[b,,]))/(P) - MU%*%t(MU)))
+    }else{
+    SIGMA<-((object@w[a]*(object@mu[a,]%*%t(object@mu[a,])+((object@nu)/(object@nu-2))*object@sigma[a,,])+object@w[b]*(object@mu[b,]%*%t(object@mu[b,])+((object@nu)/(object@nu-2))*object@sigma[b,,]))/(P) - MU%*%t(MU))*((object@nu-2)/object@nu)
+    }
+    object@w[a]<-P; object@w<-object@w[-b];
+    object@mu[a,]<-MU;
+    dims<-dim(object@mu);
+    object@z[,a] <- object@z[,a] + object@z[,b]
+    object@sigma[a,,]<-SIGMA;
+	sigma<-object@sigma[-b,,,drop=FALSE];
+	if(!is.array(sigma)&dy==1){
+		#One dimensional case.. need to make this a proper array
+		object@sigma<-array(data=sigma,c(py-1,dy,dy))
+	}
+    #if sigma is a single covariance matrix, then set it up into an appropriate array
+    if(length(dim(sigma))==2){
+        d<-dim(object@sigma);
+        object@sigma<-array(object@sigma,dim=c(1,d[2],d[3]));
+    }else{
+	object@sigma<-sigma
+	}
+    if(dims[1]==2){
+	#Removed the t(), added drop=FALSE. Cleaner
+      object@mu <- ((object@mu[-b,,drop=FALSE]));
+	  object@z <- (object@z[,-b,drop=FALSE]);
+    }else if(dims[2]==1){
+		object@mu<-(object@mu[-b,,drop=FALSE])
+		object@z <- object@z[,-b,drop=FALSE];
+	}else {
+      object@mu<-object@mu[-b,,drop=FALSE];
+      object@z <- object@z[,-b,drop=FALSE];
+    }
+  
+    
+
+
+    # update K, ICL
+    object@K <- object@K-1
+    
+    object@entropy <- -2*sum(object@z*log(object@z,base=2), na.rm=T)  
+    object@ICL <- object@BIC + 2*sum(object@z*log(object@z),na.rm=T)
+    return(object)
+}
+
+.computeDeltaE<-function(z,a,b){
+	list(a=a,b=b,e=-2*sum(z[,c(a,b)]*log(z[,c(a,b)]),na.rm=T)+2*sum(rowSums(z[,c(a,b)])*log(rowSums(z[,c(a,b)])),na.rm=T));
+}
+
+.compMD<-function(object,a,b){
+    list(a=a,b=b,e=mahalanobis(object@mu[a,],object@mu[b,],object@sigma[b,,])+mahalanobis(object@mu[b,],object@mu[a,],object@sigma[a,,]))
+}
+
+.combFunc1<-function(x,y){
+    list(x,y)[[which.max(c(x$e,y$e))]]
+}
+.combFunc2<-function(x,y){
+    list(x,y)[[which.min(c(x$e,y$e))]]
+}
+
+mergeClusters <- function(object,metric="entropy") {
+	K <- object@K
+	minEnt <- Inf
+	maxPLL<- -Inf
+	a<-0;
+	b<-0;
+	if(length(grep("multicore",loadedNamespaces()))==1&length(grep("doMC",loadedNamespaces()))==1)
+	{
+		doMC::registerDoMC();
+
+		if(metric=="entropy"){
+			resObject<-foreach (a = 1:(K-1),.combine=.combFunc1, .options.multicore=list(preschedule=TRUE))%dopar%{ 
+				foreach (b = (a+1):K,.combine=.combFunc1,.options.multicore = list(preschedule=TRUE)) %dopar%{
+					.computeDeltaE(object@z,a,b);
+				}
+
+			}
+		}
+		else if(metric=="mahalanobis"){
+			resObject<-foreach (a = 1:(K-1),.combine=.combFunc2, .options.multicore=list(preschedule=TRUE))%dopar%{ 
+				foreach (b = (a+1):K,.combine=.combFunc2,.options.multicore = list(preschedule=TRUE)) %dopar%{
+					.compMD(object,a,b);
+				}
+			}
+		}    
+		return(resObject)
+	} else {
+			if(metric=="entropy"){
+				resObject<-foreach (a = 1:(K-1),.combine=.combFunc1, .options.multicore=list(preschedule=TRUE))%do%{ 
+					foreach (b = (a+1):K,.combine=.combFunc1,.options.multicore = list(preschedule=TRUE)) %do%{
+						.computeDeltaE(object@z,a,b);
+					}
+				}
+			}
+			else if(metric=="mahalanobis"){
+				resObject<-foreach (a = 1:(K-1),.combine=.combFunc2, .options.multicore=list(preschedule=TRUE))%do%{ 
+					foreach (b = (a+1):K,.combine=.combFunc2,.options.multicore = list(preschedule=TRUE)) %do%{
+						.compMD(object,a,b);
+					}
+				}
+			}    
+			return(resObject);	
+		}
+	}
+
+BIC <- function(x)as.numeric(unlist(lapply(x,function(q)try(q@BIC,silent=TRUE))))
+ICL <- function(x)as.numeric(unlist(lapply(x,function(q)try(q@ICL,silent=TRUE))))
+ENT <-function(x)as.numeric(unlist(lapply(x,function(q)try(q@entropy,silent=TRUE))))
+NENT<-function(x)as.numeric(unlist(lapply(x,function(q)try(q@entropy/(q@K*dim(q@z)[1]),silent=TRUE))))
+
+#x is the "name" of the merged list of models.
+#y is the index of the "best" merged model
+#Returns a function that will plot the merge tree, given the index of the parameter, and the complete merge tree at m[[1]]@mtree
+ptree<-function(x,y){
+  
+  lm<-length(get(x,.GlobalEnv));
+  popnames<-rownames(get(x,.GlobalEnv)[[y]]@mu)
+  mytree<-get(x,.GlobalEnv)[[1]]@mtree
+colmeans<-NULL
+  for(i in 2:lm){
+    colmeans<-rbind(colmeans,get(x,.GlobalEnv)[[i]]@mu[,,drop=FALSE]);
+  }
+colmeans<-rbind(matrix(get(x,.GlobalEnv)[[1]]@mu,nrow=1),colmeans)
+rn<-rownames(colmeans);rn[1]<-paste(rownames(colmeans)[2:3],collapse=":");rownames(colmeans)<-rn;
+
+colors<-apply(colmeans,2,function(x){
+rgb(red=x/max(x),blue=0.5,green=0.5,alpha=0.9)
+})
+rownames(colors)<-rownames(colmeans)
+colnames(colors)<-as.vector(parameters(get(x,.GlobalEnv)[[1]]@DATA[["1"]])@data$desc[which(colnames(get(x,.GlobalEnv)[[1]]@DATA[["1"]])%in%get(x,.GlobalEnv)[[1]]@varNames)])
+#top nodes
+a<-rownames(unique(do.call(rbind,lapply(get(x,.GlobalEnv)[1:(y-1)],function(x)x@mu[,,drop=FALSE]))))
+b<-rownames(unique(do.call(rbind,lapply(get(x,.GlobalEnv)[y:lm],function(x)x@mu[,,drop=FALSE]))))
+topnodes<-(setdiff(a,b))
+if(y<lm){
+b<-rownames(unique(do.call(rbind,lapply(get(x,.GlobalEnv)[(y+1):lm],function(x)x@mu[,,drop=FALSE]))))
+a<-rownames(unique(do.call(rbind,lapply(get(x,.GlobalEnv)[1:y],function(x)x@mu[,,drop=FALSE]))))
+ttopnodes<-c(intersect(a,b),setdiff(a,b))
+}
+sg1<-subGraph(rownames(get(x,.GlobalEnv)[[y]]@mu),get(x,.GlobalEnv)[[1]]@mtree)
+sg2<-subGraph(rownames(get(x,.GlobalEnv)[[1]]@mu),get(x,.GlobalEnv)[[1]]@mtree)
+
+sgList<-list(list(graph=sg1),list(graph=sg2));
+
+#Set dashed lines for nodes below flowMerge model nodes
+nlty<-rep(1,length(nodes(mytree)))
+names(nlty)<-nodes(mytree)
+nlty[which(!names(nlty)%in%c(popnames,topnodes))]<-2
+
+#set thick lines for flowMerge model nodes
+nlwd<-rep(1,length(nodes(mytree)))
+names(nlwd)<-nodes(mytree)
+nlwd[which(names(nlwd)%in%c(popnames))]<-2
+
+shp<-rep("circle",length(nodes(mytree)))
+names(shp)<-nodes(mytree)
+shp[which(names(shp)%in%c(popnames))]<-"ellipse"
+shp[which(names(shp)%in%c(topnodes))]<-"rectangle"
+
+#Returns a function that plots this tree
+function(i,T=get(x,.GlobalEnv)[[1]]@mtree){
+lm<-length(get(x,.GlobalEnv))
+nodeDataDefaults(T,"MFI")<-NA
+  nodeDataDefaults(T,"p")<-NA
+  mfi<-unique(do.call(rbind,sapply(1:lm,function(i){get(x,.GlobalEnv)[[i]]@mu[,,drop=FALSE]})))
+  colnames(mfi)<-as.vector(parameters(get(x,.GlobalEnv)[[1]]@DATA[["1"]])@data$desc[which(colnames(get(x,.GlobalEnv)[[1]]@DATA[["1"]])%in%get(x,.GlobalEnv)[[1]]@varNames)])
+  p<-unique(do.call(c,sapply(1:lm,function(i){get(x,.GlobalEnv)[[i]]@w})))
+  names(p)<-rownames(mfi)
+  #Assign mfi and p to the nodes of the graph
+  for(j in 1:nrow(mfi)){
+	nodeData(T,rownames(mfi)[j],"MFI")<-list(mfi[j,,drop=FALSE])
+	nodeData(T,rownames(mfi)[j],"p")<-list(p[j])
+  }
+if(exists("ttopnodes")){
+	T<-subGraph(ttopnodes,T)
+}  
+
+nodeRenderInfo(T)<-list(lty=nlty)
+nodeRenderInfo(T)<-list(shape=shp)
+nodeRenderInfo(T)<-list(lwd=nlwd)
+nodeRenderInfo(T)<-list(label="")
+nodeRenderInfo(T)<-list(fill=(colors[,i]))
+
+T<-layoutGraph(T,layoutType="dot",attrs=list(graph=list(rankdir="TB",main=colnames(colors)[i],rank="source",page=c(8.5,11))),subGList=sgList)
+renderGraph(T)
+#Return the tree too
+return(T)
+}
+}
+
+
+
